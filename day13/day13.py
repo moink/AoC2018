3ebf1038cd6bf3603cdfdb6f5f5e97c5ae4cf8a8
moink <theresa.robinson@gmail.com>
2021-12-26 16:08:44 +0100

    refactor part 1

--- day13/day13.py
+++ day13/day13.py
@@ -50,33 +50,25 @@ INTERSECTION_MAP = {
     ("<", 2): "^",
 }
 
+DIRECTION_MAP = {
+    "v": (0, 1),
+    "^": (0, -1),
+    ">": (1, 0),
+    "<": (-1, 0),
+}
 
 
 def main():
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
-    data = process_input(data)
-    print('Part 1:', run_part_1(data))
-    print('Part 2:', run_part_2(data))
-
-
-def process_input(data):
-    return data
+    curr_map = advent_tools.read_input_no_strip()
+    print('Part 1:', run_part_1(curr_map))
+    print('Part 2:', run_part_2(curr_map))
 
 
 def run_part_1(curr_map):
     underlying_map, carts = get_underlying_map(curr_map)
     crash = None
     while crash is None:
-        curr_map, crash, carts = take_one_step(curr_map, underlying_map, carts)
+        crash, carts = take_one_step(underlying_map, carts)
     return crash
 
 
@@ -90,72 +82,27 @@ def get_underlying_map(start_pos):
         for i, char in enumerate(line):
             row.append(char_map[char])
             if char in MINE_MARKERS:
-                carts[(i, j)] = 0
+                carts[(i, j)] = (char, 0)
         map.append("".join(row))
     return map, carts
 
 
-def take_one_step(start_pos, underlying_map, old_carts):
-    n_cols = len(start_pos[0])
-    result = [[char for char in line] for line in start_pos]
-    carts = {}
-    for i in range(n_cols):
-        for j, line in enumerate(start_pos):
-            char = line[i]
-            if char == "v":
-                result[j][i] = underlying_map[j][i]
-                next_char = result[j + 1][i]
-                if next_char in MINE_MARKERS:
-                    result[j + 1][i] = "X"
-                    return ["".join(line) for line in result], (i, j + 1), None
-                if next_char == "+":
-                    fill_char = INTERSECTION_MAP[(char, old_carts[(i, j)])]
-                    carts[(i, j + 1)] = (old_carts[(i, j)] + 1) % 3
-                else:
-                    fill_char = CART_STEP_MAP[(char, next_char)]
-                    carts[(i, j + 1)] = old_carts[(i, j)]
-                result[j + 1][i] = fill_char
-            if char == "^":
-                result[j][i] = underlying_map[j][i]
-                next_char = result[j - 1][i]
-                if next_char in MINE_MARKERS:
-                    result[j - 1][i] = "X"
-                    return ["".join(line) for line in result], (i, j - 1), None
-                if next_char == "+":
-                    fill_char = INTERSECTION_MAP[(char, old_carts[(i, j)])]
-                    carts[(i, j - 1)] = (old_carts[(i, j)] + 1) % 3
-                else:
-                    fill_char = CART_STEP_MAP[(char, next_char)]
-                    carts[(i, j - 1)] = old_carts[(i, j)]
-                result[j - 1][i] = fill_char
-            if char == ">":
-                result[j][i] = underlying_map[j][i]
-                next_char = result[j][i + 1]
-                if next_char in MINE_MARKERS:
-                    result[j][i + 1] = "X"
-                    return ["".join(line) for line in result], (i + 1, j), None
-                if next_char == "+":
-                    fill_char = INTERSECTION_MAP[(char, old_carts[(i, j)])]
-                    carts[(i + 1, j)] = (old_carts[(i, j)] + 1) % 3
-                else:
-                    fill_char = CART_STEP_MAP[(char, next_char)]
-                    carts[(i + 1, j)] = old_carts[(i, j)]
-                result[j][i + 1] = fill_char
-            if char == "<":
-                result[j][i] = underlying_map[j][i]
-                next_char = result[j][i - 1]
-                if next_char in MINE_MARKERS:
-                    result[j][i - 1] = "X"
-                    return ["".join(line) for line in result], (i - 1, j), None
-                if next_char == "+":
-                    fill_char = INTERSECTION_MAP[(char, old_carts[(i, j)])]
-                    carts[(i - 1, j)] = (old_carts[(i, j)] + 1) % 3
-                else:
-                    fill_char = CART_STEP_MAP[(char, next_char)]
-                    carts[(i - 1, j)] = old_carts[(i, j)]
-                result[j][i - 1] = fill_char
-    return ["".join(line) for line in result], None, carts
-
+def take_one_step(underlying_map, old_carts):
+    carts = old_carts.copy()
+    for (i, j), (char, int_count) in old_carts.items():
+        delta_i, delta_j = DIRECTION_MAP[char]
+        ii = i + delta_i
+        jj = j + delta_j
+        next_char = underlying_map[jj][ii]
+        if (ii, jj) in carts:
+            return (ii, jj), None
+        del carts[(i, j)]
+        if next_char == "+":
+            fill_char = INTERSECTION_MAP[(char, int_count)]
+            carts[(ii, jj)] = (fill_char, (int_count + 1) % 3)
+        else:
+            carts[(ii, jj)] = CART_STEP_MAP[(char, next_char)], int_count
+    return None, carts
 
 
 def run_part_2(data):
--- day13/test_day13.py
+++ day13/test_day13.py
@@ -8,7 +8,7 @@ class TestCollisions(unittest.TestCase):
     def test_get_underlying_map_vertical(self):
         start_pos = ["|", "v", "|", "|", "|", "^", "|"]
         expec_map = ["|", "|", "|", "|", "|", "|", "|"]
-        expec_carts = {(0, 1): 0, (0, 5): 0}
+        expec_carts = {(0, 1): ("v", 0), (0, 5): ("^", 0)}
         calc_map, carts = day13.get_underlying_map(start_pos)
         self.assertEqual(expec_map, calc_map)
         self.assertEqual(expec_carts, carts)
@@ -16,7 +16,7 @@ class TestCollisions(unittest.TestCase):
     def test_get_underlying_map_horizontal(self):
         start_pos = ["->---<-"]
         expec_map = ["-------"]
-        expec_carts = {(1, 0): 0, (5, 0): 0}
+        expec_carts = {(1, 0): (">", 0), (5, 0): ("<", 0)}
         calc_map, carts = day13.get_underlying_map(start_pos)
         self.assertEqual(expec_map, calc_map)
         self.assertEqual(expec_carts, carts)
@@ -47,37 +47,31 @@ class TestCollisions(unittest.TestCase):
 
     def test_vert_track_step1(self):
         start_pos = ["|", "v", "|", "|", "|", "^", "|"]
-        expec_end = ["|", "|", "v", "|", "^", "|", "|"]
-        expec_carts = {(0, 2): 0, (0, 4): 0}
+        expec_carts = {(0, 2): ("v", 0), (0, 4): ("^", 0)}
         underlying_map, carts = day13.get_underlying_map(start_pos)
-        end_pos, crash, carts = day13.take_one_step(start_pos, underlying_map, carts)
-        self.assertEqual(expec_end, end_pos)
+        crash, carts = day13.take_one_step(underlying_map, carts)
         self.assertEqual(None, crash)
         self.assertEqual(expec_carts, carts)
 
     def test_vert_track_crash(self):
         start_pos = ["|", "|", "v", "|", "^", "|", "|"]
-        expec_end = ["|", "|", "|", "X", "|", "|", "|"]
         underlying_map, carts = day13.get_underlying_map(start_pos)
-        end_pos = day13.take_one_step(start_pos, underlying_map, carts)
-        self.assertEqual((expec_end, (0, 3), None), end_pos)
+        end_pos = day13.take_one_step(underlying_map, carts)
+        self.assertEqual(((0, 3), None), end_pos)
 
     def test_horiz_track_step1(self):
         start_pos = ["->---<-"]
-        expec_end = ["-->-<--"]
-        expec_carts = {(2, 0): 0, (4, 0): 0}
+        expec_carts = {(2, 0): (">", 0), (4, 0): ("<", 0)}
         underlying_map, carts = day13.get_underlying_map(start_pos)
-        end_pos, crash, carts = day13.take_one_step(start_pos, underlying_map, carts)
-        self.assertEqual(expec_end, end_pos)
+        crash, carts = day13.take_one_step(underlying_map, carts)
         self.assertEqual(None, crash)
         self.assertEqual(expec_carts, carts)
 
     def test_horiz_track_crash(self):
         start_pos = ["-->-<--"]
-        expec_end = ["---X---"]
         underlying_map, carts = day13.get_underlying_map(start_pos)
-        end_pos = day13.take_one_step(start_pos, underlying_map, carts)
-        self.assertEqual((expec_end, (3, 0), None), end_pos)
+        end_pos = day13.take_one_step(underlying_map, carts)
+        self.assertEqual(((3, 0), None), end_pos)
 
 class TestLongerExample(unittest.TestCase):
 
@@ -107,152 +101,12 @@ class TestLongerExample(unittest.TestCase):
             r"  \------/    ",
         ]
         underlying_map, _ = day13.get_underlying_map(start_pos)
-        carts = {(3, 0): 0, (9, 4): 0}
-        expec_carts = {(4, 0): 0, (10, 4): 0}
-        end_pos, crash, carts = day13.take_one_step(
-            prev_pos, underlying_map, carts
-        )
-        self.assertEqual(expec_end, end_pos)
+        _, carts = day13.get_underlying_map(prev_pos)
+        expec_carts = {(4, 0): ('v', 0), (10, 4): ('>', 0)}
+        crash, carts = day13.take_one_step(underlying_map, carts)
         self.assertEqual(None, crash)
         self.assertEqual(expec_carts, carts)
 
-    def test_intermediate_steps(self):
-        steps = [
-            [
-                r"/->-\         ",
-                r"|   |  /----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | v  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/-->\         ",
-                r"|   |  /----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \->--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---v         ",
-                r"|   |  /----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+>-/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   v  /----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+->/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----\ ",
-                r"| /->--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+--^ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----\ ",
-                r"| /-+>-+-\  | ",
-                r"| | |  | |  ^ ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----\ ",
-                r"| /-+->+-\  ^ ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----< ",
-                r"| /-+-->-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /---<\ ",
-                r"| /-+--+>\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /--<-\ ",
-                r"| /-+--+-v  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /-<--\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | v  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /<---\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \-<--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  v----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  \<+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----\ ",
-                r"| /-+--v-\  | ",
-                r"| | |  | |  | ",
-                r"\-+-/  ^-+--/ ",
-                r"  \------/    ",
-            ],
-            [
-                r"/---\         ",
-                r"|   |  /----\ ",
-                r"| /-+--+-\  | ",
-                r"| | |  X |  | ",
-                r"\-+-/  \-+--/ ",
-                r"  \------/    ",
-            ],
-        ]
-        underlying_map, carts = day13.get_underlying_map(steps[0])
-        for prev_pos, expec_end in zip(steps[0:-2], steps[1:-1]):
-            end_pos, crash, carts = day13.take_one_step(
-                prev_pos, underlying_map, carts
-            )
-            self.assertEqual(expec_end, end_pos)
-            self.assertEqual(None, crash)
-        prev_pos = steps[-2]
-        expec_end = steps[-1]
-        end_pos, crash, carts = day13.take_one_step(
-            prev_pos, underlying_map, carts
-        )
-        self.assertEqual(expec_end, end_pos)
-        self.assertEqual((7, 3), crash)
 
     def test_part_1_example(self):
         start_pos = [
