75c5041538302626de29234e19cf9365c017b73b
moink <theresa.robinson@gmail.com>
2021-12-27 11:50:37 +0100

    day 7 cleaned up

--- day7/day7.py
+++ day7/day7.py
@@ -1,34 +1,15 @@
 import contextlib
 import collections
 import copy
-import functools
-import itertools
-import math
-import re
-import statistics
-
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 
 import advent_tools
 
 
 def main():
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
-    # data = advent_tools.read_nparray_from_digits()
-    data = process_input(data)
-    print('Part 1:', run_part_1(copy.deepcopy(data)))
-    print('Part 2:', run_part_2(data, num_workers=5))
+    input_data = advent_tools.read_input_lines()
+    dependencies = process_input(input_data)
+    print('Part 1:', run_part_1(copy.deepcopy(dependencies)))
+    print('Part 2:', run_part_2(dependencies, num_workers=5))
 
 
 def process_input(data):
@@ -57,16 +38,11 @@ def run_part_1(dependencies):
 
 def run_part_2(dependencies, num_workers=2):
     not_started = {val for dep in dependencies.values()
-                for val in dep}.union(dependencies.keys())
+                   for val in dep}.union(dependencies.keys())
     completion_times = collections.defaultdict(set)
-    available = {step for step in sorted(not_started) if not dependencies[step]}
+    available = set()
+    completion_times[0] = set()
     tick = 0
-    for worker in range(num_workers):
-        if available:
-            to_do = sorted(available)[0]
-            not_started.remove(to_do)
-            available.remove(to_do)
-            completion_times[ord(to_do) - 4 + tick].add(to_do)
     while completion_times:
         tick = sorted(completion_times)[0]
         not_started = not_started.difference(completion_times[tick])
@@ -85,8 +61,6 @@ def run_part_2(dependencies, num_workers=2):
                 not_started.remove(to_do)
                 completion_times[ord(to_do) - 4 + tick].add(to_do)
     return tick
-
-
 
 
 if __name__ == '__main__':
